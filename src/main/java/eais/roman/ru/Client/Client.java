f4ddf22b4680b726f6e7951f1fd8c46bfd264bf2
jade55 <jade555@mail.ru>
2015-09-01 22:30:21 +0500

    01-09-2015 Вторник(Дом)
    Начало проекта. Проба пера

---
+++ src/main/java/eais/roman/ru/Client/Client.java
@@ -0,0 +1,14 @@
+package eais.roman.ru.Client;
+
+/**
+ * Created by 1 on 01.09.2015.
+ * @author abutkin
+ * @version 0.1
+ */
+public class Client {
+    public static void main(String[] args) {
+        // TODO: Пишем туту код
+        // FIXME: А потом правим
+
+    }
+}
---
+++ src/main/java/eais/roman/ru/Server/Server.java
@@ -0,0 +1,12 @@
+package eais.roman.ru.Server;
+
+/**
+ * Created by 1 on 01.09.2015.
+ * @author abutkin
+ * @version 0.1
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
